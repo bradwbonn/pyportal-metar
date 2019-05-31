6a63422561b62b11345dba976accd8f2427f017f
Brad Bonn <github@theotter.net>
2019-05-31 13:08:40 -0400

    Fixed API key for WX and updated README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pyportal-metar
 Display airport METAR on Adafruit PyPortal IoT display device
+Themed after Star Trek: TNG LCARS displays
+Currently pulls the most recent METAR text for a hard-coded ICAO code airfield
+
+### You will need:
+An adafruit pyPortal or other compatible device with 320x240 display
+Your secrets.py file configured with:
+* Your wifi settings
+* aio username and key
+* API key for CheckWX (get one for free at https://www.checkwx.com)
+
+## code.py
+Main code which auto-executes
+
+## /images
+Zip file of .bmp images used by the app.  Unzip content into /images
+
--- code.py
+++ code.py
@@ -1,54 +1,194 @@
 #!/usr/bin/env python
 
-import time, board, requests
+import time, board, digitalio, audioio, re
+from random import randint
+import adafruit_esp32spi.adafruit_esp32spi_requests as requests
 from adafruit_pyportal import PyPortal
-import xml.etree.ElementTree as ET
+from analogio import AnalogIn
 
+print("PyPortal METAR System Restarting...")
+
+#### Initialize public variables ####
 # Set up where we'll be fetching data from - prelim hard-code
-DATA_SOURCE = "https://www.aviationweather.gov/adds/dataserver_current/"
-DATA_PARAMS = "httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=KASH%20KCON%20KFIT"
+airport_icao = 'kash'
+checkwx_metar_url = "https://api.checkwx.com/metar/{0}/decoded".format(airport_icao)
+checkwx_metar_api_key = secrets['checkwx_token']
+checkwx_header = {'X-API-Key': check_wx_metar_api_key}
+# JSON Fields to get from URL
+raw_metar_text = ['data',0,'raw_text']
+station_id = ['data',0,'icao']
+flight_category = ['data',0,'flight_category']
+# light sensor
+analogin = AnalogIn(board.LIGHT)
+# data refresh times (minutes of the hour)
+metar_refresh = 15
+taf_refresh = 0
+# init data state holders
+field_data = dict()
+last_metar_time = 0
+# Display "mode" - defaults to METAR (chosen with left buttons)
+display_mode = 'METAR'
+# "Scan mode" which rotates through various reports
+scan_mode = False
+#### END INITIALIZE PUBLIC VARIABLES ####
 
+
+#### SET FILE LOCATIONS ####
 # the current working directory (where this file is)
 cwd = ("/"+__file__).rsplit('/', 1)[0]
+# Directory containing .wav files
+sounddir = (cwd+"/sounds")
+# images directory
+imagedir = (cwd+"/images")
+# Visuals for METAR display
+metar_bg = imagedir+"/lcars_pyportal_metar.bmp"
+taf_bg = imagedir+"/lcars_pyportal_taf.bmp"
+error_bg = imagedir+"/lcars_pyportal_offline.bmp"
+metar_font = cwd+"/fonts/Okuda-27.bdf"
+#### END SET FILE LOCATIONS ####
 
-def create_json_path(xmlinput):
+
+#### FUNCTION DEFINITIONS ####
+
+# Play the "command systems offline" sound effect
+def play_offline_audio():
+    offline = (sounddir+"/offline.wav")
+    pyportal.play_file(offline)
+
+# Choose from beeps 1-20 or 0 for random to playback
+def play_panel_beep(beep):
+    if 1 <= beep <= 20:
+        fileno = beep
+    elif beep == 0:
+        fileno = randint(1,20)
+    else:
+        print("Invalid sound selection. Playing error sound")
+        fileno = 27
+    soundfile = (sounddir+"/panel_beeps/2{:02d}.wav".format(fileno))
+    pyportal.play_file(soundfile)
+
+# Scheduler that only pulls a new METAR from the API based upon the refresh threshold
+def time_for_new_metar(last_metar_time):
+    # We haven't gotten it at all yet
+    if last_metar_time == 0:
+        return True
+    # we're on the minute-based threshold AND it's longer than the threshold away
+    elif ((time.localtime().tm_min % metar_refresh) == 0) and ((time.time() - last_metar_time) >= 60):
+        return True
+    else: # Not time to update the METAR yet
+        return False
+
+# Helper: Assemble URL with parameters since on-board CircuitPython can't do params...
+def assemble_url(url, params):
+    separator = '&'
+    parameters_strings = []
+    for param in params.keys():
+        parameters_strings.append(param+"="+str(params[param]))
+    full_metar_url = url + separator.join(parameters_strings)
+    print("URL: "+full_metar_url)
+    return full_metar_url
+
+## Loading from file only
+def get_current_metar():
+    # TEMP - open XML file
+    metar_file = 'testmetar.xml'
+    print("Opening local METAR file for testing")
+    f = open(metar_file,'r')
+    metars = extract_metar_from_xml(f)
+    f.close()
+    return metars
+
+# change background image to match local flying conditions
+def update_background(weather):
     pass
 
-def get_current_metar(url, params):
-    try:
-        r = requests.get(url+params)
-        if r.headers['content-type'] == 'application/xml':
-            xml_response = r.text
-        else:
-            print("URL returned wrong data type")
-            return None
-    except:
-        print ("Can't obtain METAR from API: ",e)
-        return None
+# Use data dictionary of text fields to update the content of the display
+def update_screen_text(data):
+    pass
 
-pyportal = PyPortal(url=DATA_SOURCE,
-                    json_path=(METAR_LOCATION, CODE_LOCATION),
-                    status_neopixel=board.NEOPIXEL,
-                    default_bg=cwd+"/images/capesuzette.bmp",
-                    text_font=cwd+"/fonts/Arial-ItalicMT-17.bdf",
-                    image_resize=(320,240),
-                    image_position=(0,0),
-                    text_position=((20, 120),  # METAR location
-                                   (5, 210)), # airport code location
-                    text_color=(0xFFFFFF,  # METAR text color
-                                0x8080FF), # airport code text color
-                    text_wrap=(35, # characters to wrap for METAR
-                               0), # no wrap for airport code
-                    text_maxlen=(180, 30), # max text size for both
+# Helper: return local time string
+def pretty_datetime(timestamp):
+    timestring = "{0}/{1}".format(
+        time.localtime(timestamp).tm_mon,
+        time.localtime(timestamp).tm_mday
+    )
+    return timestring
+
+# Helper: make clock string
+def pretty_clock(timestamp):
+    timestring = "{:02d}:{:02d}:{:02d}".format(
+        time.localtime(timestamp).tm_hour,
+        time.localtime(timestamp).tm_min,
+        time.localtime(timestamp).tm_sec
+        )
+    return timestring
+
+# Set screen brightness
+def screen_brightness():
+    pyportal.set_backlight(.8)
+
+## Run scheduled activities
+#def run_scheduler(last_metar_time):
+#    
+#    # Every minute, update the clock on-screen
+#    if time.localtime().tm_sec % 59:
+#        pyportal.set_text(pretty_lock(time.time()), index=1)
+#        
+#    # Every 15 minutes, update the METAR
+#    if last_metar_time == 0:
+#        response = pyportal.fetch()
+#        play_panel_beep(2)
+#
+#    # we're on the minute-based threshold AND it's longer than the threshold away
+#    elif ((time.localtime().tm_min % metar_refresh) == 0) and ((time.time() - last_metar_time) >= 60):
+#        response = pyportal.fetch()
+#        play_panel_beep(2)
+        
+
+#### END FUNCTION DEFINITIONS ####
+
+#### INIT PORTAL ####
+pyportal = PyPortal(
+                        url=checkwx_metar_url,
+                        headers=checkwx_header,
+                        json_path=(raw_metar_text,station_id,flight_category),
+                        status_neopixel=board.NEOPIXEL,
+                        default_bg=metar_bg,
+                        text_font=metar_font,
+                        image_resize=(320,240),
+                        image_position=(0,0),
+                        text_position=((100,100),(250,12),(90,228)),
+                        text_wrap=(30,0,0),
+                        text_maxlen=(180,30,30),
+                        text_color=(0xFFCC99,0x9999FF,0x000000)
                    )
+#### END INIT PORTAL ####
+# Play boot-up sound
+play_panel_beep(20)
 
-# speed up projects with lots of text by preloading the font!
+# Update device local time and preload the font
 pyportal.preload_font()
+pyportal.get_local_time()
+screen_brightness()
 
+#### Primary screen operation loop ####
 while True:
+    # Primary functions inside exception handler with 1-minute timeout.
     try:
-        value = pyportal.fetch()
-        print("Response is", value)
-    except RuntimeError as e:
-        print("Some error occured, retrying! -", e)
-    time.sleep(60*5) # Update every 5 minutes
+        # Run any scheduled activities
+        #run_scheduler(last_metar_time)
+        
+        # When screen is touched:
+        if pyportal.touchscreen.touch_point:
+            play_panel_beep(0)
+        # If a new METAR is scheduled to be obtained
+        if time_for_new_metar(last_metar_time):
+            response = pyportal.fetch()
+            play_panel_beep(2)
+            last_metar_time = time.time()
+    except Exception as e:
+        print("Some error occured, retrying in 1 minute: ", e)
+        pyportal.set_background(error_bg)
+        pyportal.set_text('LCARS',index=2)
+        play_offline_audio()
+        time.sleep(60)
